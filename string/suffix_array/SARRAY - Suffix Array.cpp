b7e9f62ae66e212eb3c50a5ad2dbd3e460ba235f
prince_of_crows <tamjidjasir@gmail.com>
2023-05-16 20:23:32 +0600

    2D suffix array test

--- string/suffix_array/SARRAY - Suffix Array.cpp
+++ string/suffix_array/SARRAY - Suffix Array.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define mx 300005
-#define int long long
+//#define int long long
 #define pii pair <int, int>
 #define piii pair <int, pii>
 #define fi first
@@ -17,11 +17,17 @@ using namespace std;
 #define read() freopen("in.txt", "r", stdin)
 #define write() freopen("out.txt", "w", stdout)
 #define fst ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 100005
+#define K 22
+
+using namespace std;
 
 struct suffix {
 	int idx;
 	int rank[2];
 };
+
+suffix suffixes[K][MAXN];
 
 int comp(suffix a, suffix b) {
 	return (a.rank[0] == b.rank[0]) ?
@@ -31,50 +37,54 @@ int comp(suffix a, suffix b) {
 
 vector <int> buildSuffixArray(string str) {
 	int n = str.length();
-	suffix suffixes[n];
 
 	for (int i = 0; i < n; i++) {
-		suffixes[i].idx = i;
-		suffixes[i].rank[0] = str[i] - 'a';
-		suffixes[i].rank[1] = ((i + 1) < n) ? (str[i + 1] - 'a') : -1;
+		suffixes[0][i].idx = i;
+		suffixes[0][i].rank[0] = str[i] - 'a';
+		suffixes[0][i].rank[1] = ((i + 1) < n) ? (str[i + 1] - 'a') : -1;
 	}
 
-	sort(suffixes, suffixes + n, comp);
+	sort(suffixes[0], suffixes[0] + n, comp);
 
-	int ind[n];
-	for (int j = 4; j < 2 * n; j = j * 2) {
+	int ind[n], k = 1;
+	for (int j = 4 ; j < 2 * n; j = j * 2) {
 		int rank = 0;
-		int prevR = suffixes[0].rank[0];
+		int prevR = suffixes[k-1][0].rank[0];
 
-		suffixes[0].rank[0] = rank;
-		ind[suffixes[0].idx] = 0;
+		suffixes[k][0].rank[0] = rank;
+		suffixes[k][0].idx = suffixes[k-1][0].idx;
+
+		ind[suffixes[k][0].idx] = 0;
 
 		for (int i = 1; i < n; i++) {
-			if (suffixes[i].rank[0] == prevR &&
-			    suffixes[i].rank[1] == suffixes[i - 1].rank[1]
+			if (suffixes[k-1][i].rank[0] == prevR &&
+			    suffixes[k-1][i].rank[1] == suffixes[k-1][i - 1].rank[1]
 			    ) {
-				prevR = suffixes[i].rank[0];
-				suffixes[i].rank[0] = rank;
+				prevR = suffixes[k-1][i].rank[0];
+				suffixes[k][i].rank[0] = rank;
 			} else {
-				prevR = suffixes[i].rank[0];
-				suffixes[i].rank[0] = ++rank;
+				prevR = suffixes[k-1][i].rank[0];
+				suffixes[k][i].rank[0] = ++rank;
 			}
 
-			ind[suffixes[i].idx] = i;
+			suffixes[k][i].idx = suffixes[k-1][i].idx;
+			ind[suffixes[k][i].idx] = i;
 		}
 
 		for (int i = 0; i < n; i++) {
-			int nextIdx = suffixes[i].idx + j / 2;
-			suffixes[i].rank[1] = (nextIdx < n) ?
-			                      suffixes[ind[nextIdx]].rank[0] : -1;
+			int nextIdx = suffixes[k][i].idx + j / 2;
+			suffixes[k][i].rank[1] = (nextIdx < n) ?
+			                         suffixes[k][ind[nextIdx]].rank[0] : -1;
 		}
 
-		sort(suffixes, suffixes + n, comp);
+		sort(suffixes[k], suffixes[k] + n, comp);
+
+		k++;
 	}
 
 	vector <int> sa;
 	for (int i = 0; i < n; i++)
-		sa.push_back(suffixes[i].idx);
+		sa.push_back(suffixes[k-1][i].idx);
 
 	return sa;
 }
