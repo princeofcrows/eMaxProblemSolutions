efa19e1adf515dd479ee557a0a97180a330c1cca
prince_of_crows <tamjidjasir@gmail.com>
2023-05-28 14:35:19 +0600

    ...

--- string/suffix_array/TANGLED - Entangled Strings.cpp
+++ string/suffix_array/TANGLED - Entangled Strings.cpp
@@ -20,38 +20,6 @@ using namespace std;
 #define write() freopen("out.txt", "w", stdout)
 #define fst ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-int ara[mx*2], sz[mx*2];
-
-void initset(int n){
-	for(int i = 1; i<=n; i++) {
-		ara[i] = i;
-		sz[i] = 1;
-	}
-}
-
-int findset(int i){
-	if(ara[i] == i) return i;
-	return ara[i] = findset(ara[i]);
-}
-
-bool issameset(int i, int j){
-	return (findset(i)==findset(j));
-}
-
-void unionset(int i, int j){
-	int a = findset(j);
-	int b = findset(i);
-	if(a==b) return;
-	if(sz[a]>=sz[b]) {
-		sz[a]+=sz[b];
-		ara[b] = a;
-	}
-	else{
-		sz[b]+=sz[a];
-		ara[a] = b;
-	}
-}
-
 struct suffix {
 	int idx;
 	int rank[2];
@@ -145,31 +113,66 @@ vector <int> kasai(string txt, vector <int> sa) {
 }
 
 void solve(string s, string t, int l) {
-	string txt = s+ "#" + t;
+	string txt = s + "#" + t;
 	vector <int> sa = buildSuffixArray(txt);
 	vector <int> lcp = kasai(txt, sa);
 
 	int n = s.length();
-	vector <int> cntS(l+1, 0), cntT(l+1, 0), cnt(l+1, 0);
+	vector <int> cntS(l+1, 0), cntT(l+1, 0);
+	set <int> ss, st;
 
-	for(int i=0; i<sa.size()-1; i++) {
-		int j = sa[i];
-		int l = sa[i+1];
+	for(int i=1; i<sa.size(); i++) {
+		int j = sa[i-1];
+		int x = lcp[i];
 
-		if(j > n && l < n) {
-			cnt[lcp[i]]++;
-		} else if(j < n && l > n) {
-			cnt[lcp[i]]++;
-		} else if(j < n && l < n) {
+		if(x==0) {
+			for(auto y: ss) {
+				for(int i=min(y, l); i>=0; i--) {
+					cntS[i]++;
+				}
+			}
 
+			for(auto y: st) {
+				for(int i=l; i>=0; i--) {
+					if(i >= y)
+						cntT[y] += cntS[i];
+					else
+						cntT[i] += cntS[i];
+				}
+			}
+
+			for(int i=0; i<=l; i++) cntS[i] = 0;
+
+			ss.clear();
+			st.clear();
 		} else {
-
+			if(j < n ) {
+				ss.insert(j);
+			} else {
+				st.insert(j);
+			}
 		}
 	}
 
+	for(auto y: ss) {
+		for(int i=min(y, l); i>=0; i--) {
+			cntS[i]++;
+		}
+	}
+
+	for(auto y: st) {
+		for(int i=l; i>=0; i--) {
+			if(i >= y)
+				cntT[y] += cntS[i];
+			else
+				cntT[i] += cntS[i];
+		}
+	}
+
+
 	for(int i=1; i<=l; i++) {
 		if(i > n) cout << 0 << " ";
-		else cout << cnt[i] <<  " ";
+		else cout << cntT[i] <<  " ";
 	}
 
 	cout << "\n";
